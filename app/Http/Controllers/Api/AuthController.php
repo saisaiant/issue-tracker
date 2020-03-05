3882f181dc943651250f0aa30c6099f1a27c94b2
Sai Aung Naing Tun <saisaiant@gmail.com>
2020-03-05 11:52:01 +0630

    register and login api route using passport

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,13 +8,23 @@ use App\Http\Requests\UserRegisterRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\Client;
 
 class AuthController extends Controller
 {
     public function login(UserLoginRequest $request)
     {
-        
-        return response()->json(["data" => $request->all()]);
+        $passwordGrandClient = Client::where('password_client', 1)->first();
+        $data = [
+            'grant_type' => 'password',
+            'client_id' => $passwordGrandClient->id,
+            'client_secret' => $passwordGrandClient->secret,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '*'
+        ];
+        $tokenRequest = Request::create('/oauth/token', 'post', $data);
+        return app()->handle($tokenRequest);
     }
 
     public function register(UserRegisterRequest $request)
